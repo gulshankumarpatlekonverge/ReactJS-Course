3b3cf38eb6192ec5824b948b78b2475fecd99b35
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-25 16:30:38 +0530

    challenge 4

--- React Site/CDN Practice/index.js
+++ React Site/CDN Practice/index.js
@@ -140,3 +140,21 @@ ReactDOM.render(
             </ul>
     </nav>, document.getElementById("challenge3")
 );
+
+// Challenge 4
+
+const challenge4 = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+ReactDOM.render(challenge4, document.getElementById("challenge4"))
+// document.getElementById("root").append(JSON.stringify(page))
