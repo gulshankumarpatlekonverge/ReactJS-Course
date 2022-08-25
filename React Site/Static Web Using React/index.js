7a9b35a7783dae0c9d1952c62eddd5f62c983112
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-25 18:36:00 +0530

    markup and pop quiz

---
+++ React Site/Static Web Using React/index.js
@@ -0,0 +1,21 @@
+
+//Challenge 5
+
+// import React from "react";
+// import ReactDOM from "react";
+
+const pageContent =(
+    <div>
+        <img src="./React-icon.svg.png" alt="react logo" height="100px" width="100px"></img>
+        <h1>Fun Fact About ReactJs</h1>
+        <ul>
+            <li>Was first realesed in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has over 100K starts on GitHub</li>
+            <li>Is maintaine by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(pageContent, document.getElementById("root"));
