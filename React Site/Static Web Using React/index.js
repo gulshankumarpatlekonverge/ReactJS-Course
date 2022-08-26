acf99770fd07836b3be3e6a1ebda85a2150e0f65
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-26 09:33:06 +0530

    styling with  CSS Classes

--- React Site/Static Web Using React/index.js
+++ React Site/Static Web Using React/index.js
@@ -132,4 +132,60 @@ function ParentChildComponent(){
 
 ReactDOM.render(<ParentChildComponent />, document.getElementById("parentChildComponent"));
 
+// Challenge 7 - styling classes
 
+function HeaderReact(){
+    return(
+        <div>
+            <nav className="nav-item">
+                <img src="./React-icon.svg.png" alt="react logo" className="img-item"></img>
+                <ul className="headerList">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+
+                    {/* <li className="li-item">Pricing</li>
+                    <li className="li-item">About</li>
+                    <li className="li-item">Contact</li> */}
+                </ul>
+            </nav>
+        </div>  
+    )
+}
+
+function MainReact(){
+    return(
+        <div className="div-item">
+            <h1>Fun Fact About ReactJs</h1>
+            <ul>
+                <li>Was first realesed in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has over 100K starts on GitHub</li>
+                <li>Is maintaine by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )    
+}
+
+function FooterReact(){
+    return(
+        <div className="footer-item">
+            <h5>Powered By Scrimba and FreeCodeCamp 2022</h5>
+        </div>
+    )
+}
+
+
+function PageContent(){
+    return(
+        <div>
+            <HeaderReact />
+            <MainReact />
+            <FooterReact />
+        </div>
+    )
+}
+
+
+ReactDOM.render(<PageContent />, document.getElementById("stylingRoot"));
