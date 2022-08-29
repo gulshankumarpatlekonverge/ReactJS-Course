52266585964cafcec22e3a593e3b0db4137fa5f2
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-29 19:45:10 +0530

    adding props and css fixes

--- AirBnbClone/airbnb_clone_project/src/App.js
+++ AirBnbClone/airbnb_clone_project/src/App.js
@@ -2,13 +2,44 @@ import './App.css';
 import NavBar from './NavBar';
 import BodyContent from './BodyContent';
 
+import image1 from './images/image1.jpg';
+import image2 from './images/image2.jpg';
+import image3 from './images/image3.jpg';
+import image4 from './images/image4.png';
+import image5 from './images/image5.jpg';
+
 function App() {
   return (
     <div>
       {/* NavBar Content */}
       <NavBar />
       {/* Body Content */}
-      <BodyContent />
+      <BodyContent 
+          img = {image1}
+          rating = {5}
+          reviewCount = {6}
+          country = "India"
+          title ="Indias Best Cricketer : Virat Kohli"
+          price ={158}
+      />
+
+      <BodyContent 
+          img = {image4} 
+          rating = {10}
+          reviewCount = {100}
+          country = "India"
+          title ="The Story of TCS"
+          price ={2000}
+      />
+
+      <BodyContent 
+          img = {image2}
+          rating = {5}
+          reviewCount = {6}
+          country = "India"
+          title ="Most Handsome star in Indian Cinema"
+          price ={230}
+      />
                
     </div>
   );
--- AirBnbClone/airbnb_clone_project/src/BodyContent.js
+++ AirBnbClone/airbnb_clone_project/src/BodyContent.js
@@ -1,26 +1,24 @@
 import React from 'react';
 
-import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.png';
-import image5 from './images/image5.jpg';
+// import image1 from './images/image1.jpg';
+// import image2 from './images/image2.jpg';
+// import image3 from './images/image3.jpg';
+// import image4 from './images/image4.png';
+// import image5 from './images/image5.jpg';
 import star from './images/star.png'
 
-function BodyContent(){
+function BodyContent(props){
     return(
         <div className='card'>
-          <div>
-            <img src={image1} alt='sport-1' className='card-image'></img>
+            <img src={props.img} alt='sport-1' className='card-image'></img>
             <div className='card-rating'>
               <img src={star} alt='star' className='star-card-image'></img>
-              <span>5.0</span>
-              <span>(6)</span>
-              <span>USA</span>
+              <span>{props.rating}</span>
+              <span>({props.reviewCount}) </span>
+              <span>{props.country}</span>
             </div>
-            <p className='card-content'>Image Content</p>
-            <p className='card-price'>prices $150</p>
-          </div>
+            <p className='card-content'>{props.title}</p>
+            <p className='card-price'>from ${props.price}</p>
         </div>
     )
 }
