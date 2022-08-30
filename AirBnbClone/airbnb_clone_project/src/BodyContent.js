012f96f51cd9d02dc93ad59be1cdb4cfeaa1d525
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-30 12:42:38 +0530

    adding badges

--- AirBnbClone/airbnb_clone_project/src/BodyContent.js
+++ AirBnbClone/airbnb_clone_project/src/BodyContent.js
@@ -3,11 +3,19 @@ import React from 'react';
 import star from './images/star.png'
 
 function BodyContent(props){
-    // console.log(props);
-    // console.log(props.stats)
+    let badgeText;
+    if(props.openSpots === 0){
+        badgeText = "SOLD OUT"
+    }
+    else if(props.location === "Online"){
+        badgeText = "ONLINE";
+    }
+    // else{
+    //     badgeText = props.location;
+    // }
     return(
         <div className='card'>
-            { props.openSpots === 0 && <div className="card-badge">SOLD OUT</div> }
+            { badgeText && <div className="card-badge">{badgeText}</div> }
             <img src={props.img} alt='sport-1' className='card-image'></img>
             <div className='card-rating'>
               <img src={star} alt='star' className='star-card-image'></img>
