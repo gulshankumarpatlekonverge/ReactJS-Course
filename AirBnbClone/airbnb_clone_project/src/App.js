364e995a221958387226c33b52e8f66fa1423b3d
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-30 13:15:07 +0530

    Working code using props

--- AirBnbClone/airbnb_clone_project/src/App.js
+++ AirBnbClone/airbnb_clone_project/src/App.js
@@ -4,25 +4,13 @@ import BodyContent from './BodyContent';
 import cardContentData from './BodyContentData';
 
 import image1 from './images/image1.jpg';
-import image2 from './images/image2.jpg';
-import image3 from './images/image3.jpg';
-import image4 from './images/image4.png';
-import image5 from './images/image5.jpg';
 
 function App() {
   const cardContent = cardContentData.map(value => {
       return <BodyContent 
         key = {value.id}
-        image = {image1}
-        // value = {value}
-        // spreading object as props
-        {...value}
-        
-        // stats = {value.stats}
-        // location = {value.location}
-        // title = {value.title}
-        // price= {value.price}   
-        // openSpots= {value.openSpots}  
+        img = {image1}
+        value = {value}
       />
     })
   return (
@@ -32,34 +20,7 @@ function App() {
       {/* Body Content */}
       <section className='cards-list'>
         { cardContent }
-      </section>
-      {/* <BodyContent 
-          img = {image1}
-          rating = {5}
-          reviewCount = {6}
-          country = "India"
-          title ="Indias Best Cricketer : Virat Kohli"
-          price ={158}
-      />
-
-      <BodyContent 
-          img = {image4} 
-          rating = {10}
-          reviewCount = {100}
-          country = "India"
-          title ="The Story of TCS"
-          price ={2000}
-      />
-
-      <BodyContent 
-          img = {image2}
-          rating = {5}
-          reviewCount = {6}
-          country = "India"
-          title ="Most Handsome star in Indian Cinema"
-          price ={230}
-      /> */}
-               
+      </section>       
     </div>
   );
 }
--- AirBnbClone/airbnb_clone_project/src/BodyContent.js
+++ AirBnbClone/airbnb_clone_project/src/BodyContent.js
@@ -3,29 +3,25 @@ import React from 'react';
 import star from './images/star.png'
 
 function BodyContent(props){
-    console.log(props)
     let badgeText;
-    if(props.openSpots === 0){
+    if(props.value.openSpots === 0){
         badgeText = "SOLD OUT"
     }
-    else if(props.location === "Online"){
+    else if(props.value.location === "Online"){
         badgeText = "ONLINE";
     }
-    // else{
-    //     badgeText = props.location;
-    // }
     return(
         <div className='card'>
             { badgeText && <div className="card-badge">{badgeText}</div> }
-            <img src={props.image} alt='sport-1' className='card-image'></img>
+            <img src={props.img} alt='sport-1' className='card-image'></img>
             <div className='card-rating'>
               <img src={star} alt='star' className='star-card-image'></img>
-              <span>{props.stats.rating}</span>
-              <span>({props.stats.reviewCount}) • </span>
-              <span>{props.location}</span>
+              <span>{props.value.stats.rating}</span>
+              <span>({props.value.stats.reviewCount}) • </span>
+              <span>{props.value.location}</span>
             </div>
-            <p className='card-title'>{props.title}</p>
-            <p className='card-price'>from ${props.price}</p>
+            <p className='card-title'>{props.value.title}</p>
+            <p className='card-price'>from ${props.value.price}</p>
         </div>
     )
 }
