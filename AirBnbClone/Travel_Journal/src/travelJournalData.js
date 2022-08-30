872f8c554b537ac237be808fa2aaff8c656c52fd
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-08-30 15:24:35 +0530

    Intial Setup of Solo Project

---
+++ AirBnbClone/Travel_Journal/src/travelJournalData.js
@@ -0,0 +1,52 @@
+export default [
+    {
+        id: 1,
+        title: "Waki Woods: Spend Time In Wilderness",
+        location: "Nagpur, Maharashtra, India",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "October",
+        endDate: "March",
+        description: "Popular for its lush green and tranquil environment, the Waki Woods is a perfect place for camping of a day or two with your beloved ones. Proximate to Nagpur, Waki Woods is evergreen throughout the year and offers extensive activities and opportunities ranging from archery, boat riding, horse riding, and more. It is one of the most famous places to visit near Nagpur within 100km range. In Waki Woods, you also get to admire the amazing scenic views of the greenery stretched across a large area and wildlife. Thus, this is the best place for peace seekers, nature lovers, adventure freaks, and trekkers.",
+        imageUrl: "https://img.traveltriangle.com/blog/wp-content/uploads/2019/11/Waki-Woods_28th-nov-400x229.jpg"
+    },
+    {
+        id: 2,
+        title: "Adasa: Visit Beautifully-Carved Shrines",
+        location: "Adasa, Maharashtra, India",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "June",
+        endDate: "October",
+        description: "Beautifully carved, this one among the eight Ashta-Vinayak shrines in Vidarbha is popular for its gigantic self-created Lord Ganesha. Standing in its glory, the idol of Lord Ganesha is 12 feet tall and 7 feet wide. Located in a small Adasa Village, this temple is one of the most visited tourist places near Nagpur city. Placed on the top of a small hillock, this temple with 40-50 stairs is also famous for its picturesque 360-degree bird’s eye view of the landscape",
+        imageUrl: "https://assets.traveltriangle.com/blog/wp-content/uploads/2019/11/Adasa_28th-nov.jpg"
+    },
+    {
+        id: 3,
+        title: "Pench Tiger Reserve: Witness The Wildlife",
+        location: "Pench, Maharashtra, India",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "November",
+        endDate: "June",
+        description: "Pench Tiger Reserve is a major protected tiger habitat of the world and is notified as one of the most important bird areas of India. This reserve is the “Glory of Nagpur” and is immortalized as the abode of “Bagheera” and “Mowgli” (from The Jungle Book of Rudyard Kipling). Home to beautiful Royal Bengal Tigers, birds and diverse wildlife, this reserve graced with solace offers a delightful experience to the visitors. Hence, is one of the most preferred places to visit around Nagpur.",
+        imageUrl: "https://img.traveltriangle.com/blog/wp-content/uploads/2019/04/Tiger-Kingdom.jpg"
+    },
+    {
+        id: 4,
+        title: "Chikhaldara: A Nature Lover’s Point",
+        location: "Chikhaldara, Maharashtra, India",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "October",
+        endDate: "June",
+        description: "If you are planning a nice holiday with your loved ones, then, Chikhaldara located in the Vidarbha region is worth considering. Located in the Amravati district of Maharashtra, above the 1120 meter ocean level, this hill station is well-known for is Wildlife Sanctuary. Chikhaldara is host to diverse species of animals, birds, and plants that will impress any nature lover. The Prospect Point, Devi Point, and Hurricane Point are a few famous viewpoints that offer mesmerizing views of Chikhaldara. This is not one of the best places to visit near Nagpur in summer due to its spiking temperatures.",
+        imageUrl: "https://img.traveltriangle.com/blog/wp-content/uploads/2020/02/Chikhaldara-400x235.jpg"
+    },
+    {
+        id: 5,
+        title: "Pachmarhi: Explore The Dense Forests",
+        location: "Madhya Pradesh, India",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "March",
+        endDate: "May",
+        description: "Popular for its lush green and tranquil environment, the Waki Woods is a perfect place for camping of a day or two with your beloved ones. Proximate to Nagpur, Waki Woods is evergreen throughout the year and offers extensive activities and opportunities ranging from archery, boat riding, horse riding, and more. It is one of the most famous places to visit near Nagpur within 100km range. In Waki Woods, you also get to admire the amazing scenic views of the greenery stretched across a large area and wildlife. Thus, this is the best place for peace seekers, nature lovers, adventure freaks, and trekkers.",
+        imageUrl: "https://assets.traveltriangle.com/blog/wp-content/uploads/2019/11/Pachmarhi_28th-nov.jpg"
+    }
+]
