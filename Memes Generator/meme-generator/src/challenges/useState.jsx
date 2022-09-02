eb1a9ca292b09c19c937f1a540d70ba0ac1862ab
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-02 09:51:31 +0530

    useState using call back function and quiz

--- Memes Generator/meme-generator/src/challenges/useState.jsx
+++ Memes Generator/meme-generator/src/challenges/useState.jsx
@@ -15,11 +15,29 @@ function UseStateComponent(){
 
     const [count, setCount] = React.useState(0);
     function add(){
-       setCount(count + 1);
+    /** USING CALLBACK FUNCTION:
+     * 
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+
+    //    setCount(count + 1);
+    setCount(function (oldValue){
+        return oldValue + 1;
+    })
     }
 
     function minus(){
-      setCount(count - 1);
+    //   setCount(count - 1);
+    
+    setCount(prevCount => prevCount - 1);
+
     }
     return(
         <div className="state">
