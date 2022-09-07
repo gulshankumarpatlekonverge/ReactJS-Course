e8aeb438f3a6177f40cae09b6473bf48b02f3680
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-07 11:28:51 +0530

    input type checkbox radiobutton select and submit

--- Memes Generator/meme-generator/src/challenges/Forms/formComponent.jsx
+++ Memes Generator/meme-generator/src/challenges/Forms/formComponent.jsx
@@ -6,21 +6,31 @@ function FormComponent(){
         firstName: "",
         lastName: "",
         email:  "",
-        comments: ""
+        comments: "",
+        isFriendly: true,
+        employment: "",
+        favoriteColor: ""
     });
    
     function handleChange(event){
-       console.log(event.target.value);
+       const {name, value, type, checked} = event.target;
+
        setFormData(prevFormData => {
         return{
             ...prevFormData,
-            [event.target.name]: event.target.value
+            [name]: type === "checkbox" ? checked : value
         }
        })
     }
 
+    function handleSubmit(event){
+        event.preventDefault(); // is used to hide the query string in url while page re-rendering and it doesn't reresh the page
+        // submitToApi(formData);
+        console.log(formData);
+    }
+
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
         <h1>Form Component</h1>
         <input 
             type = "text"    
@@ -46,13 +56,80 @@ function FormComponent(){
             value= {formData.email}
         />
 
-        <textarea name="textarea" 
+        <textarea name="comments" 
                 className="comments-textarea" 
                 placeholder="Enter Comments"
                 cols="30" rows="10"
                 value={formData.comments} 
                 onChange= {handleChange}
         />
+
+        <input type="checkbox" 
+                id="isFriendly" 
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            />
+        <label htmlFor="isFriendly">Are you friendly?</label>
+        <br />
+        <br />
+        <fieldset>
+                <legend>Current employment status</legend>
+                
+                <input 
+                    type="radio"
+                    id="unemployed"
+                    name="employment"
+                    value="unemployed"
+                    checked={formData.employment === "unemployed"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="unemployed">Unemployed</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="part-time"
+                    name="employment"
+                    value="part-time"
+                    checked={formData.employment === "part-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="part-time">Part-time</label>
+                <br />
+                
+                <input 
+                    type="radio"
+                    id="full-time"
+                    name="employment"
+                    value="full-time"
+                    checked={formData.employment === "full-time"}
+                    onChange={handleChange}
+                />
+                <label htmlFor="full-time">Full-time</label>
+                <br />
+                
+        </fieldset>
+        <br />
+        <select id="favColor"
+                value={formData.favoriteColor}
+                onChange={handleChange}
+                name="favoriteColor"
+        >
+            <option value="">-- Choose Color --</option>
+            <option value="red">Red</option>
+            <option value="orange">Orange</option>
+            <option value="yellow">Yellow</option>
+            <option value="green">Green</option>
+            <option value="blue">Blue</option>
+            <option value="indigo">Indigo</option>
+            <option value="violet">Violet</option>
+        </select>
+
+        {/* <input type="submit" value="submit" /> */}
+        <br />
+        <br />
+        <button>Submit</button>
 
         </form>
     )
