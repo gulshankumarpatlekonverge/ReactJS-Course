c649d7bda1738a0b7ff95fb83f829dc4b53f2d15
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-07 10:35:35 +0530

    form input object state controlled input textarea

--- Memes Generator/meme-generator/src/challenges/Forms/formComponent.jsx
+++ Memes Generator/meme-generator/src/challenges/Forms/formComponent.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import './form.css'
 
 function FormComponent(){
     const [formData, setFormData] = React.useState({
         firstName: "",
         lastName: "",
-        email:  ""
+        email:  "",
+        comments: ""
     });
    
     function handleChange(event){
@@ -19,26 +21,37 @@ function FormComponent(){
 
     return(
         <form>
-        <h1>Hello World</h1>
+        <h1>Form Component</h1>
         <input 
             type = "text"    
-            placeholder="Enter First Name"
-            onChange={handleChange}
+            placeholder= "Enter First Name"
+            onChange= {handleChange}
             name= "firstName"
+            value= {formData.firstName}
         />
 
         <input 
             type = "text"    
-            placeholder="Enter LastName"
-            onChange={handleChange}
-            name = "lastName"
+            placeholder= "Enter LastName"
+            onChange= {handleChange}
+            name= "lastName"
+            value= {formData.lastName}
         />
 
         <input 
             type = "email"    
-            placeholder="Enter Email Address"
-            onChange={handleChange}
-            name = "email"
+            placeholder= "Enter Email Address"
+            onChange= {handleChange}
+            name= "email"
+            value= {formData.email}
+        />
+
+        <textarea name="textarea" 
+                className="comments-textarea" 
+                placeholder="Enter Comments"
+                cols="30" rows="10"
+                value={formData.comments} 
+                onChange= {handleChange}
         />
 
         </form>
