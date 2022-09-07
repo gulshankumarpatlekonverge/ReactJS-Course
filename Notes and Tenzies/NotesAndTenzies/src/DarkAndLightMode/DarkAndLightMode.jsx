1b2ef115e57f224e5b6d1869aead244c3da0b3f4
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-07 19:38:28 +0530

    Dark And Light Mode Implementation

--- Notes and Tenzies/NotesAndTenzies/src/DarkAndLightMode/DarkAndLightMode.jsx
+++ Notes and Tenzies/NotesAndTenzies/src/DarkAndLightMode/DarkAndLightMode.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Main from './components/Main';
 import './DarkAndLightMode.css'
+import { useState } from "react";
 
 function DarkAndLightMode(){
+    const [lightDarkMode, setLightDarkMode] = useState(true);
+
+    function toggleLightDarkMode(){
+        setLightDarkMode(preMode => !preMode)
+    }
+
     return(
-        <div>
-            <Navbar />
-            <Main />
+        <div className="container">
+            <Navbar lightDarkMode = {lightDarkMode} toggleLightDarkMode = {toggleLightDarkMode}/>
+            <Main lightDarkMode = {lightDarkMode}/>
         </div>
     )
 }
--- Notes and Tenzies/NotesAndTenzies/src/DarkAndLightMode/components/Navbar.jsx
+++ Notes and Tenzies/NotesAndTenzies/src/DarkAndLightMode/components/Navbar.jsx
@@ -2,24 +2,15 @@ import React from "react"
 import reacticonsmall from '../images/react-icon-small.png';
 
 export default function Navbar(props) {
+    console.log(props)
     return (
-         <nav 
-            className={props.darkMode ? "dark": ""}
-        >
-            <img 
-                className="nav--logo_icon"
-                src={reacticonsmall}
-            />
+         <nav className={props.lightDarkMode ? "dark": ""}>
+            <img className="nav--logo_icon" src={reacticonsmall}/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             
-            <div 
-                className="toggler" 
-            >
+            <div className="toggler" >
                 <p className="toggler--light">Light</p>
-                <div 
-                    className="toggler--slider"
-                    onClick={props.toggleDarkMode}
-                >
+                <div className="toggler--slider" onClick={props.toggleLightDarkMode}>
                     <div className="toggler--slider--circle"></div>
                 </div>
                 <p className="toggler--dark">Dark</p>
