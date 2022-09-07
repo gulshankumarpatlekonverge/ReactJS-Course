03cd5bd380dae5269dea6347e37cc81a4ae1ba6c
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-07 16:39:34 +0530

    use dependencies using useEffect

--- Memes Generator/meme-generator/src/challenges/Forms/API Calls/apiCalls.jsx
+++ Memes Generator/meme-generator/src/challenges/Forms/API Calls/apiCalls.jsx
@@ -4,7 +4,7 @@ import React, {useState, useEffect} from "react";
 
 function MakingApiCalls(){
     const [starWarsData, setStarWarsData] = useState({});
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(1);
     // 1. GET the data (fetch)  or (axios) 
     // 2. Save the data to state
     console.log("Component Rendered")
@@ -13,20 +13,22 @@ function MakingApiCalls(){
 
     // side effects in react
 
-    useEffect(function() {
-        console.log("Effect Function Ran");
-    },[count])
+    // useEffect(function() {
+    //     console.log("Effect Function Ran");
+    // },[count])
 
     useEffect(() => {
-        fetch("https://swapi.dev/api/people/1").then(res => res.json()).then(data => setStarWarsData(data))
+        fetch("https://swapi.dev/api/people/"+ count).then(res => res.json()).then(data => setStarWarsData(data))
     }, [count])
     
 
     return(
         <div>
-            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
             <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get Next Character</button>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+            {/* <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button> */}
         </div>
     )
 }
