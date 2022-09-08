bc7ec84bb4a783e308273cd47fabfcbcbcd2f63a
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-08 14:00:33 +0530

    initial setup for TanZiesgame app

--- Notes and Tenzies/NotesAndTenzies/src/App.jsx
+++ Notes and Tenzies/NotesAndTenzies/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import DarkAndLightMode from './DarkAndLightMode/DarkAndLightMode';
 import NotesApp from './Notes App/notesApp';
+import TenziesApp from './Tenzies App/TenziesApp';
 import './App.css'
 
 function App() {
   return (
     <div>
       {/* <DarkAndLightMode /> */}
-      <NotesApp /> 
+      {/* <NotesApp />  */}
+      <TenziesApp />
     </div>
   )
 }
