91965ed463f934aaf518e49dadda84593ace84fb
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-08 09:03:15 +0530

    using localstorage to sync notes and features

--- Notes and Tenzies/NotesAndTenzies/src/App.jsx
+++ Notes and Tenzies/NotesAndTenzies/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import DarkAndLightMode from './DarkAndLightMode/DarkAndLightMode'
+import DarkAndLightMode from './DarkAndLightMode/DarkAndLightMode';
+import NotesApp from './Notes App/notesApp';
 import './App.css'
 
 function App() {
   return (
     <div>
-      <DarkAndLightMode />
+      {/* <DarkAndLightMode /> */}
+      <NotesApp /> 
     </div>
   )
 }
